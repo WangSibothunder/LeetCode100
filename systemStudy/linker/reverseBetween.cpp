f7713e9e05f337953c0bf9f4c7ff377db5bc6f07
Thunder Wang <145625357+WangSibothunder@users.noreply.github.com>
2026-02-13 22:15:14 +0800

    mac

--- systemStudy/linker/reverseBetween.cpp
+++ systemStudy/linker/reverseBetween.cpp
@@ -1,69 +1,28 @@
-#include "link.h"
-#include <cstdio>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution
-{
+#include"link.h"
+class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int left, int right)
-    {
-        ListNode *dummy = new ListNode(0);
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int cnt = 1;
+        ListNode* dummy = new ListNode(0);
         dummy->next = head;
-        int cnt = 0;
-        if(left==right) return dummy->next;
-        ListNode *reverseStart=new ListNode(0);//反转起点
-        ListNode *reverseEnd=new ListNode(0);//反转终点
-        ListNode *pre = dummy;
-        ListNode *cur = head;
-        while (cur!=nullptr&&cnt<=right)
+        ListNode* cur = dummy;
+        if(left==right) return head;
+        ListNode* reverseStart,reverseEnd;
+        while (head!=nullptr&&cnt!=right+1)
         {
+            ListNode* temp = cur;
             ListNode* nxt = cur->next;
-            if(cnt==left){
-                pre->next = reverseEnd;//反转起点前一个指向反转终点
-                reverseStart->val = cur->val;
-                pre = reverseStart;
-                cur = nxt;
-                //reverseStart.next最后赋值
-            } else if (cnt<=right&&cnt>left)
-            {
-                cur->next = pre;
-                pre = cur;
-                cur = nxt;
-                if(cnt==right){
-                    reverseEnd->val = pre->val;
-                    reverseEnd->next = pre->next;
-                    reverseStart->next = cur;
-                }
-            } else{
-                pre = cur;
-                cur = nxt;
+            if(cnt==left-1){
+                //这个时候是要反转节点前一个节点
+                reverseStart = temp;
             }
-            cnt++;
+            else if(cnt>=left&&cnt<=right){
+                //这个时候执行反转逻辑
+                if(cnt==left) {
+                    
+                }
+            }
         }
-        return dummy->next;
-        }
+        
+    }
 };
-// int main()
-// {
-//     printf("hello world\n");
-//     fflush(stdout);  // 立即刷新输出缓冲区
-//     printf("before creatList\n");
-//     fflush(stdout);
-//     // Solution sol;
-//     ListNode *head = new ListNode(0);
-//     head = creatList({1, 2, 3, 4, 5});
-//     printf("after creatList\n");
-//     fflush(stdout);
-//     // int left = 2, right = 4;
-//     // ListNode *res = sol.reverseBetween(head, left, right);
-//     // printList(head);
-//     return 0;
-// }
