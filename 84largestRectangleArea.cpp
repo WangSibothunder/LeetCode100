7b0a3480c3d6401a883b9c3d10d73cf2c35fe0f9
Thunder Wang <145625357+WangSibothunder@users.noreply.github.com>
2026-05-16 09:47:57 +0800

    5.16

---
+++ 84largestRectangleArea.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+   public:
+    int largestRectangleArea(vector<int>& heights) {}
+};
--- CodeWounder/backtracking/42trap.cpp
+++ CodeWounder/backtracking/42trap.cpp
@@ -8,8 +8,26 @@ class Solution {
         int area = 0;
         stack<int> minStack;
         for (int i = 0; i < height.size(); i++) {
-            while (minStack.size() > 1 && height[i] > height[minStack.top()]) {
+            while (minStack.size() > 1 && height[i] >= height[minStack.top()]) {
+                int curIndex = minStack.top();
+                minStack.pop();
+                int leftIndex = minStack.top();
+                int high = min(height[i], height[leftIndex]) - height[curIndex];
+                int lenth = i - leftIndex - 1;
+                area += high * lenth;
+                printf("cur: %d,left: %d,right: %d,high: %d,len: %d\n",
+                       height[curIndex], height[leftIndex], height[i], high,
+                       lenth);
+            }
+            if (minStack.empty() || height[i] < height[minStack.top()])
+                minStack.push(i);
+
+            else {
+                while (!minStack.empty() && height[i] >= height[minStack.top()])
+                    minStack.pop();
+                minStack.push(i);
             }
         }
+        return area;
     }
 };
