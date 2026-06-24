451815c11fae18c6cc6334fae86d27401d8d8022
Thunder Wang <145625357+WangSibothunder@users.noreply.github.com>
2026-06-24 18:55:57 +0800

    6.24

---
+++ withoutHint/394decodeString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+class Solution {
+   public:
+    string decodeString(string s) {
+        stack<int> timeStack;
+        stack<string> strStack;
+        bool nums = 0;
+    }
+};
